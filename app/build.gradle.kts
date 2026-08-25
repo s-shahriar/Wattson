936c02ce0351d9b4962d346480614b168e74c69e
Sayed Shahriar <shahriar@biznovelty.com>
2026-08-25 12:30:55 +0600

    Bump to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.syed.wattson"
         minSdk = 31
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.1"
+        versionCode = 8
+        versionName = "1.5.0"
     }
 
     signingConfigs {
